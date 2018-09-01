92495e6c3e1edbaa2d54febfc1dcf39611795eef
MaxReinisch <mreinisc@oberlin.edu>
2018-09-01 15:46:47 -0700

    changed repo name.  Also fixed setup issues and added logging

--- setup.py
+++ setup.py
@@ -1,24 +1,52 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# '''
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+# USA.
+# '''
 
-setup(
-    name='Article2TVNews',
-    version='0.1.0',
-    author='Max W. Reinisch',
-    author_email='reinischmax@gmail.com',
-    packages=['article2tvnews', 'article2tvnews.test'],
-    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='https://github.com/MaxReinisch/article2tvnews-package',
-    license='LICENSE.txt',
-    description='Recommends TV news clips related to given news articles',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "lxml==4.2.4",
-        "numpy==1.15.0",
-        "readability-lxml==0.7",
-        "requests==2.19.1",
-        "scikit-learn==0.19.2",
-        "scipy==1.1.0",
-        "sklearn==0.0",
-        "urllib3==1.23"
-    ],
-)
+import sys
+import setuptools
+
+deps = [
+    "lxml==4.2.4",
+    "numpy==1.15.0",
+    "readability-lxml==0.7",
+    "requests==2.19.1",
+    "scikit-learn==0.19.2",
+    "scipy==1.1.0",
+    "urllib3==1.23"
+]
+
+setuptools.setup(
+        name='tvnews',
+        version='0.1.1',
+        description='Recommends TV news clips related to given news articles',
+        url='https://github.com/MaxReinisch/tvnews',
+        author='Max W. Reinisch',
+        author_email='reinischmax@gmail.com',
+        long_description=open('README.md').read(),
+        license='LICENSE.txt',
+        packages=['tvnews', 'tvnews.test'],
+        install_requires=deps,
+        zip_safe=False,
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha'
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+
+        ])
---
+++ tvnews/NewsRecommender.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3.4
+# News Recommender version 2
+# written by Max Reinisch
+
+# Given a URL for a news article, recommend TV news
+# clips similar to article contents
+
+####################
+##### IMPORTS ######
+####################
+
+import requests #TODO: use urllib3 instead of Requests
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.spatial.distance import cosine
+import logging
+
+
+
+####################
+#### VARIABLES #####
+####################
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+CALAIS_ENDPOINT = 'https://api.thomsonreuters.com/permid/calais'
+CALAIS_TOKEN = "cvTFhY53VXBYm5HO85weHPx346W05015"
+CALAIS_HEADER = {'X-AG-Access-Token' : CALAIS_TOKEN, 'Content-Type' : 'text/raw', 'outputformat' : 'application/json'}
+log = logging.getLogger(__name__)
+
+
+def makeRecommendations(article):
+    calais_json = getOpenCalaisResponse(article)
+    entities = getSearchQuery(calais_json)
+    GDELT_response = getDGELTv2Response(entities) #GDELT_response in JSON format
+    if(GDELT_response.get('clips')):
+        return list(sortClipsBySimilarity(GDELT_response.get("clips"), article))
+    else:
+        return []
+
+
+def getOpenCalaisResponse(article):
+    response = requests.post(CALAIS_ENDPOINT, data=article.get("body").encode('utf-8'), headers=CALAIS_HEADER, timeout=80)
+    if response.status_code >= 400:
+        log.error("OpenCalais returned status code: " + res.status_code+ ".  Exiting...")
+        exit(-1)
+    content = response.text
+
+    c = json.loads(content)
+    return c
+
+def getSearchQuery(c):
+    # This function is an unsolved problem.  How to find the best search query
+    # Currently returns a list of all entities' names in order of number of mentions
+    entities = [values for values in c.values() if values.get("_typeGroup") == "entities"]
+    return [e.get('lastname') or e['name'] for e in sorted(entities, key=lambda x: len(x['instances']), reverse=True)]
+
+def getDGELTv2Response(entities):
+    good_result = False
+    entities = entities[:3]
+    while(not good_result):
+        if len(entities) ==0:
+            log.warning("No search found.  Returning empty result.")
+            return {}
+        query = " ".join(['"' + entity + '"' for entity in entities])
+        url = 'https://api.gdeltproject.org/api/v2/tv/tv?query='+query+ '%20market:%22National%22&mode=clipgallery&format=json&datanorm=perc&timelinesmooth=0&datacomb=sep&last24=yes&timezoom=yes&TIMESPAN=14days#'
+        res = requests.get(url)
+
+
+        if(res.status_code>=400):
+            log.error("GDELT returned status code: " + res.status_code+ ".  Exiting...")
+            exit(-1)
+        gdelt_json = json.loads(res.text)
+        if len(gdelt_json.keys()) ==0:
+            entities = entities[0:-1]
+            log.info("GDLET returned 0 results. Simplifying search to: " + entities)
+        else:
+            good_result = True
+    return gdelt_json
+
+
+def sortClipsBySimilarity(clips, article):
+    vectorizer = TfidfVectorizer(stop_words="english", ngram_range=(1,2) )
+    bow = vectorizer.fit_transform([clip.get('snippet') for clip in clips])
+    article_bow = vectorizer.transform([article.get('body')])
+    cosine_distances = [cosine(vec.todense(), article_bow.todense()) for vec in bow]
+    return zip(clips, cosine_distances)
