1db01d09d65e44665fd204c9b3d82ca4d5464da8
Anish Sarangi <anish@archive.org>
2025-11-19 16:53:03 +0530

    update the version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 setuptools.setup(
         name='tvnews',
-        version='0.2.6',
+        version='0.2.7',
         description='Recommends TV news clips related to given news articles',
         url='https://github.com/internetarchive/tvnews',
         author='Max W. Reinisch',
