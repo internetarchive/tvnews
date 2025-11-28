9a277c04fa742fa77723bc137988535a40912f91
Anish Sarangi <anish@archive.org>
2025-11-29 03:35:25 +0530

    Remove minimum versions, allow numpy 2.x. Keep urllib3>=1.26.16.

--- setup.py
+++ setup.py
@@ -32,12 +32,12 @@ setuptools.setup(
         packages=['tvnews'],
         install_requires=[
             "readability-lxml>=0.8.1",
-            "scikit-learn>=1.0.0",
-            "scipy>=1.7.0",
-            "numpy>=1.21.0,<2",
-            "spacy>=3.7.5,<3.8.0",
+            "scikit-learn",
+            "scipy",
+            "numpy",
+            "spacy",
             "urllib3>=1.26.16",
-            "en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.1/en_core_web_sm-3.7.1.tar.gz"
+            "en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0.tar.gz"
         ],
         tests_require=[
             'pytest',
